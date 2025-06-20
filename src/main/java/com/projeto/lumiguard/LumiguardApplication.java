5c79b70f33b2079a695433826a03a6d793be6f58
Alexsandro Marques <alexoliveira.marques@gmail.com>
2025-06-20 01:35:12 -0300

    primeiro commit, estrutura do projeto

---
+++ src/main/java/com/projeto/lumiguard/LumiguardApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.lumiguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LumiguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LumiguardApplication.class, args);
+	}
+
+}
